76eb6f7f5affaf526712b3bf5649ab86cfa009ff
Chandrakant Solanki <chandrkant.solanki@stellingtech.com>
2018-03-21 10:30:54 +0530

    change in code

--- app/adapters/project.js
+++ app/adapters/project.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
 	namespace: '',
-	host: "http://localhost:3000",
+	host: "http://localhost:3030",
 	pathForType(){
 		return 'projects'
 	}
