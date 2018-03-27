346dca9c0fe4e02034646adbed3d60cf6ed2c93b
Chandrakant Solanki <chandrkant.solanki@stellingtech.com>
2018-03-27 11:48:08 +0530

    add js file

--- app/adapters/user.js
+++ app/adapters/user.js
@@ -2,5 +2,8 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
 	namespace: '',
-	host: "http://localhost:3030"
+	host: "http://localhost:3030",
+	pathForType(){
+		return 'users'
+	}
 });
